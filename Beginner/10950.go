fdc4a31c4301c1b4f7f4dab4b6013533c6ae253c
velbie <velbie13@gmail.com>
2021-03-26 16:52:44 +0900

    .

---
+++ Beginner/10950.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var t int
+	fmt.Scanf("%d\n", &t)
+	for {
+		var a, b int
+		fmt.Scanf("%d %d\n", &a, &b)
+		fmt.Printf("%d\n", a+b)
+		t = t - 1
+		if t == 0 {
+			return
+		}
+	}
+}
---
+++ Beginner/2558.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scanf("%d \n", &a)
+	fmt.Scanf("%d \n", &b)
+	fmt.Printf("%d", a+b)
+}
