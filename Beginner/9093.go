65ad4be60c27625ffb2aee8282a3c2557ba8d26a
velbie <velbie13@gmail.com>
2021-03-29 18:55:40 +0900

    .

--- Beginner/9093.go
+++ Beginner/9093.go
@@ -11,16 +11,23 @@ func main() {
 	writer := bufio.NewWriter(os.Stdout)
 	var t int
 	fmt.Fscanf(reader, "%d\n", &t)
-	// var stack []string
+	var stack []string
 	for {
-		// var s string
-		// fmt.Fscanln(reader, &s)
-		// input, _, _ := reader.ReadLine()
-		input, _ := reader.ReadString('\n')
-		// input = strings.TrimSpace(input)
-		for i, s := range input {
-			fmt.Fprintf(writer, "%d: %s\n", i, string(s))
+		input, _, _ := reader.ReadLine()
+		input = append(input, '\n')
+		for _, s := range input {
+			if string(s) == " " || string(s) == "\n" {
+				for j, _ := range stack {
+					fmt.Fprint(writer, string(stack[len(stack)-(j+1)]))
+				}
+				fmt.Fprintf(writer, " ")
+				writer.Flush()
+				stack = nil
+			} else {
+				stack = append(stack, string(s))
+			}
 		}
+		fmt.Fprintf(writer, "\n")
 		writer.Flush()
 		t = t - 1
 		if t == 0 {
